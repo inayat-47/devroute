de0c775891e3842a094c82fd76d6e823556f18ad
Faiz Khan <68123004+devfaizzz@users.noreply.github.com>
2026-08-31 18:51:21 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   # Vite dev server
         "http://localhost:3000",   # Alternate dev port
+        "https://devroute.vercel.app", #deploy
     ],
     allow_credentials=True,
     allow_methods=["*"],           # Allow all HTTP methods (GET, POST, etc.)
